0cc213eb0c1c3648bf675b7a561a690efb124524
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-01-04 23:34:45 +0530

    navigation drawer

--- src/ts/global.ts
+++ src/ts/global.ts
@@ -9,6 +9,7 @@
  * Imports
  */
 import { ripple } from "./utils/ripple";
+import { addEventOnElements } from "./utils/event";
 
 
 /**
@@ -29,6 +30,20 @@ window.addEventListener("scroll", () => {
 const /** {NodeListOf<Element>} */ $rippleElements: NodeListOf<Element> | null = document.querySelectorAll("[data-ripple]");
 
 $rippleElements.forEach($rippleElement => ripple($rippleElement as HTMLElement));
+
+
+/**
+ * Navbar toggle for mobile screen
+ */
+
+const $navToggler = document.querySelectorAll("[data-nav-toggler]");
+const $navbar = document.querySelector("[data-navigation]");
+const $scrim = document.querySelector("[data-scrim]");
+
+addEventOnElements($navToggler, "click", function () {
+  $navbar?.classList.toggle("show");
+  $scrim?.classList.toggle("active");
+});
 
 
 /**
