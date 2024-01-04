ae474fc1a0fcade182316bb1dfdd25c5e15985c2
Basavarajakjbasuu <basavarajakj06@gmail.com>
2024-01-04 19:33:17 +0530

    Ripple effect added 🤦‍♂️🤦‍♂️

--- src/ts/global.ts
+++ src/ts/global.ts
@@ -4,3 +4,28 @@
  */
 
 "use strict";
+
+/**
+ * Imports
+ */
+import { ripple } from "./utils/ripple";
+
+
+/**
+ * Header on scroll state
+ */
+
+const /** {HTMLElement} */ $header = document.querySelector('[data-header]');
+
+window.addEventListener("scroll", () => {
+  $header?.classList[window.scrollY > 50 ? "add": "remove"]("active");
+});
+
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeListOf<Element>} */ $rippleElements: NodeListOf<Element> | null = document.querySelectorAll("[data-ripple]");
+
+$rippleElements.forEach($rippleElement => ripple($rippleElement as HTMLElement));
